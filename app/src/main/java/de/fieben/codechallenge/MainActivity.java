54533525c1370643bf342d03df104015ce6a1b46
Felix Moser <fmo@sinnerschrader-mobile.de>
2014-08-22 21:50:42 +0200

    Added GooglePlayServices and extended BaseGameActivity in MainAcivity.

--- app/src/main/java/de/fieben/codechallenge/MainActivity.java
+++ app/src/main/java/de/fieben/codechallenge/MainActivity.java
@@ -1,36 +1,30 @@
 package de.fieben.codechallenge;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.example.games.basegameutils.BaseGameActivity;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseGameActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private TextView mText;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		mText = (TextView) findViewById(R.id.text);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onSignInFailed() {
+		mText.setText("Failed. :(");
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		mText.setText("Succeeded! :D");
+	}
 }
